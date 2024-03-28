7bca2e11fcdb27879c1a5477604d5dabfb53df83
zhrbit <iamzahrabit@gmail.com>
2024-03-28 11:57:32 +0700

    web full

---
+++ pweb_11/script.js
@@ -0,0 +1,39 @@
+const title = document.querySelector('.title')
+const leaf1 = document.querySelector('.leaf1')
+const leaf2 = document.querySelector('.leaf2')
+const bush2 = document.querySelector('.bush2')
+const mount1 = document.querySelector('.mount1')
+const mount2 = document.querySelector('.mount2')
+const sun = document.querySelector('.sun')
+
+document.addEventListener('scroll', function() {
+    let value = window.scrollY
+    // console.log(value)
+    title.style.marginTop = value * 1.1 + 'px'
+
+    leaf1.style.marginLeft = -value + 'px'
+    leaf2.style.marginLeft = value + 'px'
+
+    bush2.style.marginBottom = -value + 'px'
+
+    mount1.style.marginBottom = -value * 1.1 + 'px'
+    mount2.style.marginBottom = -value * 1.2 + 'px'
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+    var scrollLinks = document.querySelectorAll('.scroll-link');
+
+    scrollLinks.forEach(function (link) {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            var targetId = this.getAttribute('href').substring(1);
+            var targetElement = document.getElementById(targetId);
+
+            window.scrollTo({
+                top: targetElement.offsetTop - 50, // Sesuaikan dengan kebutuhan Anda
+                behavior: 'smooth'
+            });
+        });
+    });
+});
